4537c9dd4f364254b530f62301b0a5e1b7365e56
yangwei999 <348134071@qq.com>
2023-03-10 10:45:56 +0800

    add and find pr

---
+++ softwarepkg/infrastructure/pullrequestimpl/repository.go
@@ -0,0 +1,56 @@
+package pullrequestimpl
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/opensourceways/server-common-lib/utils"
+	"gopkg.in/yaml.v3"
+
+	"github.com/opensourceways/robot-gitee-software-package/softwarepkg/domain"
+)
+
+const prDir = "pr-storage"
+
+func (impl *pullRequestImpl) Add(pr *domain.PullRequest) error {
+	data, err := yaml.Marshal(pr)
+	if err != nil {
+		return err
+	}
+
+	fileName, err := impl.genFileName(pr.Pkg.Id)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, data, 0644)
+}
+
+func (impl *pullRequestImpl) Find(pkgId int) (pr domain.PullRequest, err error) {
+	fileName, err := impl.genFileName(strconv.Itoa(pkgId))
+	if err != nil {
+		return
+	}
+
+	_, err = os.Stat(fileName)
+	if err != nil {
+		return
+	}
+
+	if err = utils.LoadFromYaml(fileName, &pr); err != nil {
+		return
+	}
+
+	return
+}
+
+func (impl *pullRequestImpl) genFileName(id string) (string, error) {
+	if s, err := os.Stat(prDir); err != nil || !s.IsDir() {
+		if err = os.Mkdir(prDir, 755); err != nil {
+			return "", err
+		}
+	}
+
+	return fmt.Sprintf("%s/%s.yaml", prDir, id), nil
+}
