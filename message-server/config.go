3faf080729971ed027f5e9474fc982ebe1b8a06f
yangwei999 <348134071@qq.com>
2023-03-13 10:17:36 +0800

    approved pkg

--- message-server/config.go
+++ message-server/config.go
@@ -6,6 +6,7 @@ type Config struct {
 }
 
 type Topics struct {
-	NewPkg   string `json:"new_pkg"    required:"true"`
-	CIPassed string `json:"ci_passed"  required:"true"`
+	CIPassed            string `json:"ci_passed"                    required:"true"`
+	ApplyingSoftwarePkg string `json:"applying_software_pkg"        required:"true"`
+	ApprovedSoftwarePkg string `json:"approved_software_pkg"        required:"true"`
 }
--- message-server/message.go
+++ message-server/message.go
@@ -1,9 +1,34 @@
 package messageserver
 
-import "github.com/opensourceways/robot-gitee-software-package/softwarepkg/app"
+import (
+	"regexp"
+	"strconv"
+
+	"github.com/opensourceways/robot-gitee-software-package/softwarepkg/app"
+)
+
+var prNumRe = regexp.MustCompile(`\d+$`)
 
 type messageOfNewPkg struct{}
 
 func (msg *messageOfNewPkg) toCmd() (app.CmdToCreatePR, error) {
 	return app.CmdToCreatePR{}, nil
+}
+
+type messageOfApprovedPkg struct {
+	PkgId      string `json:"pkg_id"`
+	PkgName    string `json:"pkg_name"`
+	RelevantPR string `json:"pr"`
+}
+
+func (msg *messageOfApprovedPkg) toCmd() (cmd app.CmdToMergePR, err error) {
+	prNumStr := prNumRe.FindString(msg.RelevantPR)
+	prNumInt, err := strconv.Atoi(prNumStr)
+	if err != nil {
+		return
+	}
+
+	cmd.PRNum = prNumInt
+
+	return
 }
