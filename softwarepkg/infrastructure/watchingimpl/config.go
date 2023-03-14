fa95685b98831e60e958fa9803d80f3c154b2739
yangwei999 <348134071@qq.com>
2023-03-14 15:08:02 +0800

    set default org

--- softwarepkg/infrastructure/watchingimpl/config.go
+++ softwarepkg/infrastructure/watchingimpl/config.go
@@ -1,11 +1,15 @@
 package watchingimpl
 
 type Config struct {
-	Org      string `json:"org" required:"true"`
+	Org      string `json:"org"`
 	Interval int    `json:"interval"`
 }
 
 func (cfg *Config) SetDefault() {
+	if cfg.Org == "" {
+		cfg.Org = "src-openeuler"
+	}
+
 	if cfg.Interval <= 0 {
 		cfg.Interval = 10
 	}
